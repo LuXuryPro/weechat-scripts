6d45c26eb61acf94d1d71d93cdd1594d99685bfc
the-godfather <radek1236987@gmail.com>
2013-11-14 00:08:52 +0100

    fixes and improvements in xnotify.py

--- python/xnotify.py
+++ python/xnotify.py
@@ -6,6 +6,9 @@ import os
 weechat.register("xnotify", "", "0.0.5", "GPL", "xmessage notification system for weechat", "", "")
 weechat.hook_print("", "irc_privmsg", "", 1, "notify_show", "")
 
+command = "xmessage -center -buttons OK:0 -default OK "
+
+
 def is_displayed(buffer):
     dp = weechat.buffer_get_integer(buffer, 'num_displayed')
     #weechat.prnt(weechat.current_buffer(),"\ndp: "+str(dp))
@@ -16,7 +19,7 @@ def is_displayed(buffer):
 
 def inactive():
     inactivity = int(weechat.info_get('inactivity', ''))
-    weechat.prnt(weechat.current_buffer(),"\nIn: "+str(inactivity))
+    #weechat.prnt(weechat.current_buffer(),"\nIn: "+str(inactivity))
     if inactivity > 20:
         return True
     else:
@@ -25,11 +28,9 @@ def inactive():
 
 def notify_show(data, bufferp, uber_empty, tagsn, isdisplayed, ishilight, prefix, message):
     l = tagsn.split(",")
-    if ishilight == "1" and not is_displayed(bufferp):
-        os.system("xmessage " + "-center " + " -buttons OK:0 "+"-default OK " +message + "&")
-    elif "notify_private" in l and not is_displayed(bufferp):
-        nick = l[-2].split("_")[-1]
-        os.system("xmessage " + "-center " + " -buttons OK:0 "+"-default OK " +nick+ ": " +message + "&")
-    #weechat.prnt(weechat.current_buffer(),data + "\n" + bufferp + "\n" + uber_empty + "\n" + tagsn + "\n" + isdisplayed + "\n" + ishilight + "\n" + prefix + "\n" + message  + "\n")
+    if ishilight == "1" or "notify_private" in l and not is_displayed(bufferp):
+        nick = prefix[1:]
+        os.system(command + nick+ ": " + message + "&")
+    weechat.prnt(weechat.current_buffer(),data + "\n" + bufferp + "\n" + uber_empty + "\n" + tagsn + "\n" + isdisplayed + "\n" + ishilight + "\n" + prefix + "\n" + message  + "\n")
     return weechat.WEECHAT_RC_OK
 
